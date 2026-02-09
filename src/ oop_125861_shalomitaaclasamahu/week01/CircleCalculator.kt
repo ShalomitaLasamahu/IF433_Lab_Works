de8f764beb10757fa12b6d6a0eebdd9cedfc8915
Mizai. <shalllommm@gmail.com>
2026-02-09 21:41:26 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/ oop_125861_shalomitaaclasamahu/week01/CircleCalculator.kt
+++ src/ oop_125861_shalomitaaclasamahu/week01/CircleCalculator.kt
@@ -1,17 +1,11 @@
 package oop_125861_shalomitaaclasamahu.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius: Double = 7.0
     val pi = 3.14
-
-    // Calculation
     val area = pi * radius * radius
 
-    // Output String T
     println("Radius: $radius , Area: $area")
-
-    // Logic check
     println(checkSize(area))
 }
 
