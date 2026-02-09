c4c9d916275b582025001f396e97b018a5075daf
Mizai. <shalllommm@gmail.com>
2026-02-09 21:00:18 +0700

    week01: add calculateStatus function

--- src/ oop_125861_shalomitaaclasamahu/week01/GradingSystem.kt
+++ src/ oop_125861_shalomitaaclasamahu/week01/GradingSystem.kt
@@ -1,4 +1,7 @@
-package ` oop_125861_shalomitaaclasamahu`.week01
+package oop_125861_shalomitaaclasamahu.week01
+
+// Tulis DI LUAR main()
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
 
 fun main() {
     // REFACTOR: Gunakan val dan hapus tipe data eksplisit
@@ -10,11 +13,14 @@ fun main() {
 
     // Tambahkan dibawah println
     val grade = when (score) {
-       in 90..100 -> "A"
+        in 90..100 -> "A"
         in 80..89 -> "B"
         in 70..79 -> "C"
         else -> "D"
     }
 
     println("Grade: $grade")
+
+    // Panggil DI DALAM main()
+    println("Status: ${calculateStatus(score)}")
 }
