dc7ec85bec51fbd7ef15454ae5035e74bd6b6904
Mizai. <shalllommm@gmail.com>
2026-02-09 21:02:07 +0700

    week01: implement null safety check

--- src/ oop_125861_shalomitaaclasamahu/week01/GradingSystem.kt
+++ src/ oop_125861_shalomitaaclasamahu/week01/GradingSystem.kt
@@ -23,4 +23,12 @@ fun main() {
 
     // Panggil DI DALAM main()
     println("Status: ${calculateStatus(score)}")
+
+    // Tambahkan di akhir main()
+    val studentId: String? = null
+
+    // Jika null, gunakan nilai default 0
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
 }
