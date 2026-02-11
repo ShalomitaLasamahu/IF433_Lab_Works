67f5f66c988000665fef01b73978152315bc1dcf
Mizai. <shalllommm@gmail.com>
2026-02-11 15:57:37 +0700

    week02: add secondary constructor

--- src/oop_125861_shalomitaclasamahu/week2/mahasiswa.kt
+++ src/oop_125861_shalomitaclasamahu/week2/mahasiswa.kt
@@ -5,5 +5,18 @@ class Student(
     val nim: String,
     var major: String
 ) {
-    // Body class kosong dulu
+
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+
+    // Secondary Constructor
+    constructor(name: String, nim: String) :
+            this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
