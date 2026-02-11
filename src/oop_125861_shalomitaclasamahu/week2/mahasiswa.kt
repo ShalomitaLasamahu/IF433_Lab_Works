0dc97ed651c140cc3d489af2fabba10534a6874c
Mizai. <shalllommm@gmail.com>
2026-02-11 15:54:37 +0700

    week02: create basic Student class structure

--- src/oop_125861_shalomitaclasamahu/week2/mahasiswa.kt
+++ src/oop_125861_shalomitaclasamahu/week2/mahasiswa.kt
@@ -5,14 +5,5 @@ class Student(
     val nim: String,
     var major: String
 ) {
-
-    init {
-        // Validasi sederhana: cek panjang NIM
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-        }
-    }
+    // Body class kosong dulu
 }
