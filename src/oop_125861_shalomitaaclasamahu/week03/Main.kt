34f1ceb545b410c9e10c7fdb1a2e98e2567b52bc
Mizai. <shalllommm@gmail.com>
2026-02-18 15:33:30 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_125861_shalomitaaclasamahu/week03/Main.kt
+++ src/oop_125861_shalomitaaclasamahu/week03/Main.kt
@@ -1,6 +1,7 @@
 package oop_125861_shalomitaaclasamahu.week03
 
 fun main() {
+
     val e = Employee("Budi")
 
     e.salary = -1000
@@ -10,4 +11,12 @@ fun main() {
     e.increasePerformance()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    val weapon = Weapon("Pedang Naga")
+
+    weapon.damage = -50      // harus gagal
+    weapon.damage = 9999     // harus jadi 1000
+
+    println("Damage Weapon: ${weapon.damage}")
+    println("Tier Weapon: ${weapon.tier}")
 }
