ec73212c8a46d97144186fbc7e9f2f290c492a87
Mizai. <shalllommm@gmail.com>
2026-02-25 15:18:17 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_125861_shalomitaaclasamahu/week04/Main.kt
+++ src/oop_125861_shalomitaaclasamahu/week04/Main.kt
@@ -11,4 +11,12 @@ fun main() {
     myCar.openTrunk() // Memanggil method milik sendiri
     myCar.honk() // Memanggil method yang sudah di-override
     myCar.accelerate() // Memanggil gabungan method parent dan child
+
+    println(" --- ELECTRIC CAR TEST ---")
+
+    val electricCar = ElectricCar("Tesla", 4, 85)
+
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
 }
