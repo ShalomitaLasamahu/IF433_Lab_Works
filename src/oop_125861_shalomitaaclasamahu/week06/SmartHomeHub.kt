473ef48d9549826725cd75e468f2ef7eb3834282
Mizai. <shalllommm@gmail.com>
2026-03-11 15:52:53 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_125861_shalomitaaclasamahu/week06/SmartHomeHub.kt
@@ -0,0 +1,34 @@
+package oop_125861_shalomitaaclasamahu.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+
+        for (device in devices) {
+
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
