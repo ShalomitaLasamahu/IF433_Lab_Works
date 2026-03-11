bca50384a899c205a3cf03ea9316dc61f41b9257
Mizai. <shalllommm@gmail.com>
2026-03-11 15:08:06 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_125861_shalomitaaclasamahu/week06/Smartphone.kt
+++ src/oop_125861_shalomitaaclasamahu/week06/Smartphone.kt
@@ -1,5 +1,11 @@
 package oop_125861_shalomitaaclasamahu.week06
 
-//  Error: Class Smartphone inherits multiple implentations of turn0n()
+
 class Smartphone: Camera, Phone {
+    // Manually ovveride to resolve ambiguity
+    override fun tur0n() {
+        super<Camera>.turn0n() // menjalankan logika camera
+        super<Phone>.tur0n() // menjalankan logika phone
+        println("Sistem operasi smartphone berhasil booting.")
+    }
 }
