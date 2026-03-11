4492a98071d6b1e9bfe9971f020ceba2ea0c9415
Mizai. <shalllommm@gmail.com>
2026-03-11 15:03:49 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_125861_shalomitaaclasamahu/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_125861_shalomitaaclasamahu.week06
+
+interface Camera{
+    fun turn0n() { println("Lensa kamera terbuka dan sensor akitf.") }
+}
+
+interface Phone {
+    fun tur0n() { println("Sinyal seluler mencari jaringan.") }
+}
