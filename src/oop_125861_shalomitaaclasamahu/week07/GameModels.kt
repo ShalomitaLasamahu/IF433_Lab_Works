113edf718b1c6b7d945977fa036b8e55cdadf0f8
Mizai. <shalllommm@gmail.com>
2026-03-25 16:01:55 +0700

    week07: (task) create GameItem data class

--- src/oop_125861_shalomitaaclasamahu/week07/GameModels.kt
+++ src/oop_125861_shalomitaaclasamahu/week07/GameModels.kt
@@ -6,4 +6,10 @@ enum class ItemRarity(val dropChance: Int) {
     RARE(7),
     EPIC(2),
     LEGENDARY(1)
+
+    data class GameItem(
+        val name: String,
+        val damage: Int,
+        val rarity: ItemRarity
+    )
 }
