938371da0ae03d7e91900b46ac1385428eb8577d
Mizai. <shalllommm@gmail.com>
2026-03-25 15:19:24 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_125861_shalomitaaclasamahu/week07/Main.kt
+++ src/oop_125861_shalomitaaclasamahu/week07/Main.kt
@@ -2,7 +2,7 @@ package oop_125861_shalomitaaclasamahu.week07
 
 import oop_125861_shalomitaaclasamahu.week07.NetworkClient.Companion.createClient
 
-fun Main() {
+fun main() {
     println("=== TEST SINGLETON ===")
     println("Status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
@@ -10,4 +10,10 @@ fun Main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient() // Instansiasi lewat Factory
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegulerUser("Amanda",19)
+    val reg2 = RegulerUser("Amanda", 19)
+    println(reg1)
+    println("Sama? ${reg1 == reg2}") // false
 }
---
+++ src/oop_125861_shalomitaaclasamahu/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_125861_shalomitaaclasamahu.week07
+
+class RegulerUser(val name: String, val age: Int)
