37ef1cf2b3b2333ba5b3126d372b3eb321bd595b
Mizai. <shalllommm@gmail.com>
2026-03-25 15:35:25 +0700

    week07: create ApiResponse sealed class

--- src/oop_125861_shalomitaaclasamahu/week07/SystemStates.kt
+++ src/oop_125861_shalomitaaclasamahu/week07/SystemStates.kt
@@ -2,4 +2,10 @@ package oop_125861_shalomitaaclasamahu.week07
 
 enum class AppStates {
     STARTTING, RUNNING, STOPPED
-}
+ }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Failure(val error: String) : ApiResponse()
+    object Loading : ApiResponse()
+ }
