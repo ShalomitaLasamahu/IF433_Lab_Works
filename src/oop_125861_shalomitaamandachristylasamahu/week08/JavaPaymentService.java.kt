3e2cd59807d27b757139b1dea867e6a184ffa558
Mizai. <shalllommm@gmail.com>
2026-04-15 16:24:35 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_125861_shalomitaamandachristylasamahu/week08/JavaPaymentService.java.kt
@@ -0,0 +1,7 @@
+package oop_125861_shalomitaamandachristylasamahu.week08
+
+object JavaPaymentService {
+    fun processPayment(productId: String): String {
+        return "TRX-" + productId + "-SUCCESS"
+    }
+}
