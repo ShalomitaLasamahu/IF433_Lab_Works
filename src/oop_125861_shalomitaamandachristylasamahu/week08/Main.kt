30f48182afdd8bdc59b322489e6f30b385d79bc0
Mizai. <shalllommm@gmail.com>
2026-04-15 16:16:15 +0700

    week08: simulate unit test failure using !!

--- src/oop_125861_shalomitaamandachristylasamahu/week08/Main.kt
+++ src/oop_125861_shalomitaamandachristylasamahu/week08/Main.kt
@@ -60,4 +60,6 @@ fun main() {
     val statusLength = javaResponse!!.length
     println("Status dari Java: $javaResponse (Length: $statusLength)")
 
+    // Panggil mock unit test dari UserTest.kt
+    runMockUnitTest()
 }
