52c9b0adaa26270397f98b2f6bb4d8cc288508a3
Mizai. <shalllommm@gmail.com>
2026-04-15 15:51:54 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_125861_shalomitaamandachristylasamahu/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_125861_shalomitaamandachristylasamahu.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
+
