a1aff446a65a8be59a19c2cd17f1ee094a17f534
Mizai. <shalllommm@gmail.com>
2026-04-29 16:00:19 +0700

    week10: (task) populate coin repository

--- src/oop_125861_shalomitamandachristylasamahu/week10/CryptoDashboard.kt
+++ src/oop_125861_shalomitamandachristylasamahu/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_125861_shalomitamandachristylasamahu.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+        coinRepo.add(Coin("BTC", 1.5))
+        coinRepo.add(Coin("ETH", 10.0))
+        coinRepo.add(Coin("USDT", 500.0))
+
 }
