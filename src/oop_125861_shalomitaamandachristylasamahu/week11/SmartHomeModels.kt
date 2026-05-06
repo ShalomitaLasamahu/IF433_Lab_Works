10057cddd856594944bdf38c8b84149855cd4bb1
Mizai. <shalllommm@gmail.com>
2026-05-06 11:47:51 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_125861_shalomitaamandachristylasamahu/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_125861_shalomitaamandachristylasamahu.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
