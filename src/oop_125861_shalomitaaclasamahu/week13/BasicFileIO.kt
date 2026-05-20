6938b313af6373c4e80def008bd409fb4a4bef38
Mizai. <shalllommm@gmail.com>
2026-05-20 09:31:55 +0700

    week13: implement readText and readLines

--- src/oop_125861_shalomitaaclasamahu/week13/BasicFileIO.kt
+++ src/oop_125861_shalomitaaclasamahu/week13/BasicFileIO.kt
@@ -7,4 +7,15 @@ fun main() {
     file.writeText("notes.txt")
     println("File berhasil dibuat dan ditulis")
 
+    file.appendText("Line 2: Menambahkan konfigurasi baruu.\n")
+    println("teks berhasil di-appendd:.")
+
+    println("\n===TEST READ TEXT ===")
+    val fullContent = file.readText()
+    println("Membaca sekaligus:\n$fullContent")
+
+    println("\n===TEST READ LINES ===")
+    val lines = file.readLines()
+    lines.forEachIndexed { index, line ->  }
+        println("Isi index $index: $line")
 }
